d0facdc62773fed2fde0c722a3bb49675662d228
Dank3m <dankimist@gmail.com>
2024-01-06 15:14:28 +0300

    Built the client module

---
+++ infrastructure/src/main/java/org/arispay/adapters/ClientJpaAdapter.java
@@ -0,0 +1,4 @@
+package org.arispay.adapters;
+
+public class ClientJpaAdapter {
+}
---
+++ infrastructure/src/main/java/org/arispay/mappers/ClientMapper.java
@@ -0,0 +1,43 @@
+package org.arispay.mappers;
+
+import org.arispay.data.ClientDto;
+import org.arispay.entity.Client;
+import org.arispay.entity.Company;
+import org.arispay.repository.CompanyRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+
+
+    static CompanyRepository getCompanyRepository() {
+        return null;
+    }
+
+    @Mapping(source = "company", target = "company", qualifiedByName = "companyToId")
+    ClientDto clientToClientDto(Client client);
+
+    @Mapping(source = "company", target = "company", qualifiedByName = "idToCompany")
+    Client clientDtoToClient(ClientDto clientDto);
+
+    @Mapping(source = "company", target = "company", qualifiedByName = "companyToId")
+    List<ClientDto> companyListToCompanyDtoList(List<Client> clientList);
+
+    @Mapping(source = "company", target = "company", qualifiedByName = "idToCompany")
+    List<Client> CompanyDtoListTocompanyList(List<ClientDto> ClientDtoList);
+
+    @Named("idToCompany")
+    public static Company idToCompany (long id) {
+        return getCompanyRepository().getReferenceById(id);
+    }
+
+    @Named("companyToId")
+    public static Long companyToId(Company company) {
+        return company.getId();
+    }
+}
