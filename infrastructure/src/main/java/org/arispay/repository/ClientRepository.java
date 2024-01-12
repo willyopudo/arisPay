2172de42759f10aa174faaf8de6dfc7aa1b1d42c
Dank3m <dankimist@gmail.com>
2024-01-12 21:47:25 +0300

    Fully implemented the validation controller for Family Bank

--- infrastructure/src/main/java/org/arispay/repository/ClientRepository.java
+++ infrastructure/src/main/java/org/arispay/repository/ClientRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
-    @Query(value = "From Client Where clientId = ?1", nativeQuery = true)
+    @Query(value = "Select * From Client Where client_Id = ?1", nativeQuery = true)
     Optional<Client> findClientByClientId(String clientId);
 }
