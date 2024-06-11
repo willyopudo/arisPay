50c335e0b319d2be010d72514db4c09c67eaaea9
willyopudo <opudowilfred.ow@gmail.com>
2024-06-11 15:18:49 +0300

    Added fbl ipn controller and log4j request logging bean

--- application-api/src/main/java/org/arispay/controller/fbl/IpnController.java
+++ application-api/src/main/java/org/arispay/controller/fbl/IpnController.java
@@ -1,7 +1,31 @@
 package org.arispay.controller.fbl;
 
+import jakarta.validation.Valid;
+import org.arispay.data.GenericHttpResponse;
+import org.arispay.data.fbl.dtorequest.confirmation.ConfirmationRequest;
+import org.arispay.data.fbl.dtorequest.ipn.FblIpnDto;
+import org.arispay.data.fbl.dtoresponse.confirmation.ConfirmationResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/fbl")
 public class IpnController {
+    @PostMapping("/ipn")
+    public ResponseEntity<GenericHttpResponse> processIpnRequest(
+            @Valid @RequestBody FblIpnDto ipnRequest) {
+        //Request logging is handled by logging configuration bean
+        //Todo
+        //Call service function to check and extract contents of payload and save to db if all specifications are met
+        GenericHttpResponse httpResponse = new GenericHttpResponse();
+        httpResponse.setHttpStatus(HttpStatus.OK);
+        httpResponse.setMessage("Transaction received successfully");
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(httpResponse);
+    }
 }
---
+++ domain/src/main/java/org/arispay/data/fbl/dtorequest/ipn/FblIpnDto.java
@@ -0,0 +1,12 @@
+package org.arispay.data.fbl.dtorequest.ipn;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class FblIpnDto {
+    private String transRef;
+}
