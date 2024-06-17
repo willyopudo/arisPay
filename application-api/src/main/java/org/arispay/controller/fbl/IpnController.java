f7aa43e17b65b999b866dbb03afc45bf7e8dd144
willyopudo <opudowilfred.ow@gmail.com>
2024-06-17 13:22:09 +0300

    Added authentication to open api swagger, exception handling for fbl ipn controller

--- application-api/src/main/java/org/arispay/controller/fbl/IpnController.java
+++ application-api/src/main/java/org/arispay/controller/fbl/IpnController.java
@@ -1,5 +1,7 @@
 package org.arispay.controller.fbl;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,6 +28,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 @RestController
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "FBL IPN", description = "IPN API for Family Bank")
 @RequestMapping("/api/v1/fbl")
 public class IpnController {
     @Autowired
@@ -43,33 +47,41 @@ public class IpnController {
             @Valid @RequestBody FblIpnDto ipnRequest) throws ParseException {
         //Request logging is handled by logging configuration bean
         if(globalHelpers.isNotBlank(ipnRequest)) {
-            for(TXN txn : ipnRequest.getIPN().getTXN()){
-                if(globalHelpers.isNotBlank(txn.TXN_ACC)){
-                    CompanyAccountDto account = companyAccountServicePort.getByAccountNumber(ipnRequest.getIPN().getTXN().getFirst().TXN_ACC);
+            try {
+                for (TXN txn : ipnRequest.getIPN().getTXN()) {
+                    if (globalHelpers.isNotBlank(txn.TXN_ACC)) {
+                        CompanyAccountDto account = companyAccountServicePort.getByAccountNumber(ipnRequest.getIPN().getTXN().getFirst().TXN_ACC);
 
-                    SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd");
-                    TransactionDto transactionDto = new TransactionDto(txn.getTXN_REF(), null, Float.parseFloat(txn.getTXN_DETAIL().getFirst().TXN_AMT), txn.getTXN_ACC(), 0L, null, null, txn.getTXN_DETAIL().getFirst().TXN_CODE, txn.getTXN_DETAIL().getFirst().TXN_NARRATION, "IPN", formatter.parse(txn.getTXN_DETAIL().getFirst().TXN_DATE), txn.getTXN_DETAIL().getFirst().TXN_TYPE);
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                        TransactionDto transactionDto = new TransactionDto(txn.getTXN_REF(), null, Float.parseFloat(txn.getTXN_DETAIL().getFirst().TXN_AMT), txn.getTXN_ACC(), 0L, null, null, txn.getTXN_DETAIL().getFirst().TXN_CODE, txn.getTXN_DETAIL().getFirst().TXN_NARRATION, "IPN", formatter.parse(txn.getTXN_DETAIL().getFirst().TXN_DATE), txn.getTXN_DETAIL().getFirst().TXN_TYPE);
 
-                    if(account != null) {
-                        logger.info("Account number was found {}", account.getAccountNumber());
-                        //Todo
+                        if (account != null) {
+                            logger.info("Account number was found {}", account.getAccountNumber());
+                            //Todo
 
-                        //Call service function to save to db if company with passed account exists
+                            //Call service function to save to db if company with passed account exists
 
-                        TransactionDto savedTrans =  transactionServicePort.addTransaction(transactionDto);
-                        logger.info("Transaction record added success : {}", savedTrans.toString());
+                            TransactionDto savedTrans = transactionServicePort.addTransaction(transactionDto);
+                            logger.info("Transaction record added success : {}", savedTrans.toString());
 
 
-                    }
-                    else{
-                        logger.info("Account number was not found {}", ipnRequest.getIPN().getTXN().getFirst().TXN_ACC);
-                        //Todo
-                        //Save to rejected Transaction table
-                        TransactionDto savedTrans =  transactionRejectedServicePort.addTransaction(transactionDto);
-                        logger.info("Transaction record added to rejected : {}", savedTrans.toString());
+                        } else {
+                            logger.info("Account number was not found {}", ipnRequest.getIPN().getTXN().getFirst().TXN_ACC);
+                            //Todo
+                            //Save to rejected Transaction table
+                            TransactionDto savedTrans = transactionRejectedServicePort.addTransaction(transactionDto);
+                            logger.info("Transaction record added to rejected : {}", savedTrans.toString());
 
+                        }
                     }
                 }
+            }
+            catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                GenericHttpResponse httpResponse = new GenericHttpResponse();
+                httpResponse.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+                httpResponse.setMessage("An error occurred while processing IPN request");
+                return ResponseEntity.internalServerError().body(httpResponse);
             }
 
         }
