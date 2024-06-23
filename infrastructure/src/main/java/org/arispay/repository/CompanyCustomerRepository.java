c559c5fafe82951795776bffc15344358c5f2de1
Dank3m <dankimist@gmail.com>
2024-06-23 18:21:39 +0300

    "Switched database from MySQL to PostgreSQL in multiple projects"

--- infrastructure/src/main/java/org/arispay/repository/CompanyCustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.arispay.repository;
-
-import org.arispay.entity.CompanyCustomer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyCustomerRepository extends JpaRepository<CompanyCustomer, Long> {
-}
