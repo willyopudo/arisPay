12d2808da472b79235f35cf68685df8bc120f884
Dank3m <dankimist@gmail.com>
2024-12-30 08:55:07 +0300

    Fixed security risk - exposed credentials

--- domain/src/main/java/org/arispay/configuration/MailConfiguration.java
+++ domain/src/main/java/org/arispay/configuration/MailConfiguration.java
@@ -1,5 +1,6 @@
 package org.arispay.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,14 +11,20 @@ import java.util.Properties;
 @Configuration
 public class MailConfiguration {
 
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("dankimist@gmail.com");
-        mailSender.setPassword("dbnsjnqvszefjnqx");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
